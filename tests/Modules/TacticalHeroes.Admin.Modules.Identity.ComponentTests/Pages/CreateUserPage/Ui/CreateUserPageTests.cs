016b82118a6dd2e56eb6b59c52c389e596bbe0d2
Mikhail Novoyarchikov <mixaila17may2003@gmail.com>
2026-09-09 10:41:24 +0400

    test: cover form back and cancel destinations

--- tests/Modules/TacticalHeroes.Admin.Modules.Identity.ComponentTests/Pages/CreateUserPage/Ui/CreateUserPageTests.cs
+++ tests/Modules/TacticalHeroes.Admin.Modules.Identity.ComponentTests/Pages/CreateUserPage/Ui/CreateUserPageTests.cs
@@ -8,6 +8,8 @@ using Microsoft.Extensions.Http;
 using MudBlazor.Services;
 
 using TacticalHeroes.Admin.Api.DependencyInjection;
+using TacticalHeroes.Admin.Shared.Ui.Forms;
+using TacticalHeroes.Admin.Shared.Ui.Layout;
 
 using CreateUserPageComponent =
     TacticalHeroes.Admin.Modules.Identity.Pages.CreateUserPage.Ui.CreateUserPage;
@@ -32,6 +34,20 @@ public sealed class CreateUserPageTests : BunitContext
                 builder => builder.PrimaryHandler = _handler));
         Services.AddIdentityAdminModule();
         JSInterop.Mode = JSRuntimeMode.Loose;
+    }
+
+    [Fact(DisplayName = "Render should link back and cancel to list when form is displayed")]
+    public void Render_Should_LinkBackAndCancelToList_When_FormIsDisplayed()
+    {
+        var component = Render<CreateUserPageComponent>();
+
+        component.WaitForAssertion(() =>
+        {
+            component.FindComponent<PageBackButton>().Find("a").GetAttribute("href")
+                .ShouldBe(IdentityRoutes.Users);
+            component.FindComponent<EditFormActions>().Find("a").GetAttribute("href")
+                .ShouldBe(IdentityRoutes.Users);
+        });
     }
 
     [Fact(DisplayName = "Submit should navigate to update page when user is valid")]
--- tests/Modules/TacticalHeroes.Admin.Modules.Identity.ComponentTests/Pages/UpdateRolePage/Ui/UpdateRolePageTests.cs
+++ tests/Modules/TacticalHeroes.Admin.Modules.Identity.ComponentTests/Pages/UpdateRolePage/Ui/UpdateRolePageTests.cs
@@ -8,6 +8,8 @@ using Microsoft.Extensions.Http;
 using MudBlazor.Services;
 
 using TacticalHeroes.Admin.Api.DependencyInjection;
+using TacticalHeroes.Admin.Shared.Ui.Forms;
+using TacticalHeroes.Admin.Shared.Ui.Layout;
 
 using UpdateRolePageComponent =
     TacticalHeroes.Admin.Modules.Identity.Pages.UpdateRolePage.Ui.UpdateRolePage;
@@ -32,6 +34,21 @@ public sealed class UpdateRolePageTests : BunitContext
                 builder => builder.PrimaryHandler = _handler));
         Services.AddIdentityAdminModule();
         JSInterop.Mode = JSRuntimeMode.Loose;
+    }
+
+    [Fact(DisplayName = "Render should link back and cancel to list when form is displayed")]
+    public void Render_Should_LinkBackAndCancelToList_When_FormIsDisplayed()
+    {
+        var component = Render<UpdateRolePageComponent>(parameters => parameters
+            .Add(page => page.Id, _roleId));
+
+        component.WaitForAssertion(() =>
+        {
+            component.FindComponent<PageBackButton>().Find("a").GetAttribute("href")
+                .ShouldBe(IdentityRoutes.Roles);
+            component.FindComponent<EditFormActions>().Find("a").GetAttribute("href")
+                .ShouldBe(IdentityRoutes.Roles);
+        });
     }
 
     [Fact(DisplayName = "Submit should navigate to list when role is valid")]
